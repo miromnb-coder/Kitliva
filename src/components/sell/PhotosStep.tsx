54eaf11a85ac64cfd6878c3a7d7c76e019008223
miromnb-coder <miromnb@icloud.com>
2026-06-11 21:39:26 +0300

    Pass selected photos into sell photos step

--- src/components/sell/PhotosStep.tsx
+++ src/components/sell/PhotosStep.tsx
@@ -1,11 +1,19 @@
 import { AiConditionCheck } from "@/components/sell/AiConditionCheck";
 import { AiTitleSuggestion } from "@/components/sell/AiTitleSuggestion";
 import { PhotoUploadPreview } from "@/components/sell/PhotoUploadPreview";
+import { SellPhoto } from "@/types/sell";
 
-export function PhotosStep() {
+type PhotosStepProps = {
+  photos: SellPhoto[];
+  error?: string | null;
+  onAddPhotos: (photos: SellPhoto[]) => void;
+  onRemovePhoto: (photoId: string) => void;
+};
+
+export function PhotosStep({ photos, error, onAddPhotos, onRemovePhoto }: PhotosStepProps) {
   return (
     <>
-      <PhotoUploadPreview />
+      <PhotoUploadPreview photos={photos} error={error} onAddPhotos={onAddPhotos} onRemovePhoto={onRemovePhoto} />
       <AiTitleSuggestion />
       <AiConditionCheck />
     </>
