810f97e7e784b2779ff83e0e8bc4ecd9434ccadd
miromnb-coder <miromnb@icloud.com>
2026-06-11 13:06:34 +0300

    Add root app layout

---
+++ src/app/_layout.tsx
@@ -0,0 +1,23 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as SystemUI from "expo-system-ui";
+
+import { colors } from "@/constants/colors";
+
+export default function RootLayout() {
+  useEffect(() => {
+    SystemUI.setBackgroundColorAsync(colors.background);
+  }, []);
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <StatusBar style="dark" />
+        <Stack screenOptions={{ headerShown: false }} />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
+}
